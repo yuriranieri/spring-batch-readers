c09226b3e519913e9e9ce1d39f7411bd90c5bbd0
yuri.ranieri <yuri.ranieri@compasso.com.br>
2022-02-08 16:02:56 -0300

    aula 30 criando reader para ArquivoDelimitado

--- ArquivoDelimitadoJob/src/main/java/com/springbatch/arquivodelimitado/reader/LeituraArquivoDelimitadoReaderConfig.java
+++ ArquivoDelimitadoJob/src/main/java/com/springbatch/arquivodelimitado/reader/LeituraArquivoDelimitadoReaderConfig.java
@@ -1,17 +1,28 @@
 package com.springbatch.arquivodelimitado.reader;
 
 import com.springbatch.arquivodelimitado.dominio.Cliente;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 
 @Configuration
 public class LeituraArquivoDelimitadoReaderConfig {
 
     @Bean
-    public FlatFileItemReader<Cliente> leituraArquivoDelimitadoReader() {
-        // TODO Implementar leitor de arquivo delimitado.
-        return null;
+    @StepScope
+    public FlatFileItemReader<Cliente> leituraArquivoDelimitadoReader(
+            @Value("#{jobParameters['arquivoClientes']}") Resource arquivoClientes) {
+        return new FlatFileItemReaderBuilder<Cliente>()
+                .name("leituraArquivoDelimitadoReader") // nome do leitor
+                .resource(arquivoClientes) // recurso que vair ler (arquivo)
+                .delimited() // tipo arquivo delimitado
+                .names("nome", "sobrenome", "idade", "email")// nomes das propriedades
+                .targetType(Cliente.class)// obj de dominio
+                .build();
     }
 
 }
