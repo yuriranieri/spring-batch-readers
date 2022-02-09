168075d09e767212c8a6a6895e3a4f12b8f4ed1d
yuri.ranieri <yuri.ranieri@compasso.com.br>
2022-02-09 14:33:51 -0300

    aula 40 Tratamento de erros de leitura - Skip

--- SkipExceptionJob/src/main/java/com/springbatch/skipexception/reader/SkipExceptionReaderConfig.java
+++ SkipExceptionJob/src/main/java/com/springbatch/skipexception/reader/SkipExceptionReaderConfig.java
@@ -6,8 +6,13 @@ import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuild
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static java.lang.String.format;
 
 @Configuration
 public class SkipExceptionReaderConfig {
@@ -18,8 +23,31 @@ public class SkipExceptionReaderConfig {
                 .name("skipExceptionReader")
                 .dataSource(dataSource)
                 .sql("select * from cliente")
-                .beanRowMapper(Cliente.class)
+                .rowMapper(rowMapper())
                 .build();
+    }
+
+    private RowMapper<Cliente> rowMapper() {
+        return new RowMapper<>() {
+            @Override
+            public Cliente mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+                if (resultSet.getRow() == 14) {
+                    throw new SQLException((format("Encerrando a execução - Cliente inválido %s",
+                            resultSet.getString("email"))));
+                } else {
+                    return clienteRowMapper(resultSet);
+                }
+            }
+
+            private Cliente clienteRowMapper(ResultSet resultSet) throws SQLException {
+                var cliente = new Cliente();
+                cliente.setNome(resultSet.getString("nome"));
+                cliente.setSobrenome(resultSet.getString("sobrenome"));
+                cliente.setIdade(resultSet.getString("idade"));
+                cliente.setEmail(resultSet.getString("email"));
+                return cliente;
+            }
+        };
     }
 
 }
